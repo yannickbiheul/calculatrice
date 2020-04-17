21aeae2c821988bc1b47b92f5daaed052f85bbe1
Yannick Biheul <yannickbiheul@outlook.fr>
2020-04-17 12:26:18 +0200

    JavaScript ecrire 1/2

--- calcul.js
+++ calcul.js
@@ -3,6 +3,17 @@ const digits = Array.from(document.querySelectorAll('.digit'));
 const operators = Array.from(document.querySelectorAll('.operator'));
 const allClear = document.querySelector('.clear');
 
+let temp = "";
+
+const nums = {
+    num1: 0,
+    num2: 0,
+    operator: null
+};
+
+let fontSize = 3;
+screen.style.fontSize = "3em";
+
 /* Utilisation de map() pour parcourir chaque entrée du tableau */
 digits.map(digit => {
     digit.addEventListener("click", saveDigit);
@@ -16,10 +27,29 @@ allClear.addEventListener("click", function() {
     console.log("clear");
 })
 
-function saveDigit() {
-    console.log(this);
+function operation() {
+    console.log(this.getAttribute("data-key"));
 }
 
-function operation() {
-    console.log(this.innerText);
+function saveDigit() {
+    const digit = this.getAttribute("data-key");
+
+    temp += digit;
+
+    write(temp)
 }
+
+function write(num) {
+    screen.innerText = formatNum(num);
+}
+
+function formatNum(num) {
+    if (screen.clientWidth > window.innerWidth * 0.8) {
+        fontSize *= 0.8;
+        screen.style.fontSize = `${fontSize}em`;
+    }
+    return parseFloat(num)
+        .toFixed(2)
+        .replace(/(\d)(?=(\d{3})+\.)/g, '$1 ')
+}
+
